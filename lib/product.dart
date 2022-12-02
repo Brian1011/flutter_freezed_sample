cd3961d2cb2a7e21f95e06cd2b0a6fba01ad0611
Brian Mutinda <mutindabrian26@gmail.com>
2022-12-02 15:40:40 +0300

    generate product freezed

--- lib/product.dart
+++ lib/product.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'product.freezed.dart';
@@ -6,7 +5,7 @@ part 'product.freezed.dart';
 @freezed
 class Product with _$Product {
   const factory Product(
-      {@required final int id,
-      @required final String name,
-      @required double? price}) = _Product;
+      {required final int id,
+      required final String name,
+      required double? price}) = _Product;
 }
